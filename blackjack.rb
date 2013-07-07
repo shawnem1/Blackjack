048bd5441532772d316ff4d741645886adac7f2b
Shawne Maynard <shawnem1@gmail.com>
2013-07-06 21:54:19 -0700

    Blackjack rev4
    Add the ability to play again.

--- blackjack.rb
+++ blackjack.rb
@@ -28,100 +28,103 @@ name = gets.chomp
 puts 'Hello, and welcome to Ruby BlackJack'
 puts "Good luck, #{name}!"
 
-#Build a deck of cards
-suits = %w(S H C D)
-cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
-deck = suits.product(cards)
+play_again = "y"
 
-player_cards = []
-dealer_cards = []
+while play_again == "y" do
 
-#Initial deal
-player_cards << deck.sample
-deck.delete(player_cards.last)
-dealer_cards << deck.sample
-deck.delete(dealer_cards.last)
-player_cards << deck.sample
-deck.delete(player_cards.last)
-dealer_cards << deck.sample
-deck.delete(dealer_cards.last)
+  #Build a deck of cards
+  suits = %w(S H C D)
+  cards = %w(2 3 4 5 6 7 8 9 10 J Q K A)
+  deck = suits.product(cards)
 
-puts "Dealer shows, #{dealer_cards[1]}"
-puts ''
-puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
-if get_total(player_cards) == 21
-  puts 'Blackjack! You win!'
-  exit
-end
+  player_cards = []
+  dealer_cards = []
 
-puts ''
-
-puts 'Would you like to 1) Hit or 2) Stand?'
-
-h_or_s = gets.chomp
-
-while h_or_s == "1" do
+  #Initial deal
   player_cards << deck.sample
   deck.delete(player_cards.last)
-
-  if get_total(player_cards) > 21
-    puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
-    puts 'You busted, dealer wins'
-    h_or_s = "2"
-    exit
-  end
-
-  if get_total(player_cards) == 21
-    puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
-    h_or_s = "2"
-  end
-
-  puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
-  puts ''
-  puts 'Would you like to 1) Hit or 2) Stand?'
-  h_or_s = gets.chomp
-
-end
-
-#Dealer now turns over other card
-puts "Dealers shows, #{dealer_cards}, for a total of #{get_total(dealer_cards)}"
-
-puts ''
-
-if get_total(dealer_cards) == 21
-  puts 'Blackjack! Dealer wins'
-  exit
-end
-
-#Dealer deals until at least 17
-while get_total(dealer_cards) < 17 do
-  puts 'Dealer now hits'
   dealer_cards << deck.sample
   deck.delete(dealer_cards.last)
-  puts "Dealers shows, #{dealer_cards}, for a total of #{get_total(dealer_cards)}"
-  puts ''
+  player_cards << deck.sample
+  deck.delete(player_cards.last)
+  dealer_cards << deck.sample
+  deck.delete(dealer_cards.last)
 
-  if get_total(dealer_cards) >= 17
-    break
+  puts "Dealer shows, #{dealer_cards[1]}"
+  puts ''
+  puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
+  if get_total(player_cards) == 21
+    puts 'Blackjack! You win!'
   end
 
-end
+  puts ''
 
-if (get_total(dealer_cards) < get_total(player_cards)) && (get_total(dealer_cards) < 22)
-  puts 'You win!'
-  exit
-end
+  puts 'Would you like to 1) Hit or 2) Stand?'
 
-if (get_total(dealer_cards) > get_total(player_cards)) && (get_total(dealer_cards) < 22)
-  puts 'Dealer wins'
-  exit
-end
+  h_or_s = gets.chomp
 
-if get_total(dealer_cards) == get_total(player_cards)
-  puts 'Push'
-  exit
-end
+  while h_or_s == "1" do
+    player_cards << deck.sample
+    deck.delete(player_cards.last)
 
-if get_total(dealer_cards) > 21
-  puts 'Dealer busts, YOU WIN!'
+    if get_total(player_cards) > 21
+      puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
+      puts 'You busted, dealer wins'
+      h_or_s = "2"
+
+    end
+
+    if get_total(player_cards) == 21
+      puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
+      h_or_s = "2"
+    end
+
+    puts "Your hand is, #{player_cards} for a total of #{get_total(player_cards)}"
+    puts ''
+    puts 'Would you like to 1) Hit or 2) Stand?'
+    h_or_s = gets.chomp
+
+  end
+
+  #Dealer now turns over other card
+  puts "Dealers shows, #{dealer_cards}, for a total of #{get_total(dealer_cards)}"
+
+  puts ''
+
+  if get_total(dealer_cards) == 21
+    puts 'Blackjack! Dealer wins'
+  end
+
+  #Dealer deals until at least 17
+  while get_total(dealer_cards) < 17 do
+    puts 'Dealer now hits'
+    dealer_cards << deck.sample
+    deck.delete(dealer_cards.last)
+    puts "Dealers shows, #{dealer_cards}, for a total of #{get_total(dealer_cards)}"
+    puts ''
+
+    if get_total(dealer_cards) >= 17
+      break
+    end
+
+  end
+
+  if (get_total(dealer_cards) < get_total(player_cards)) && (get_total(dealer_cards) < 22)
+    puts 'You win!'
+  end
+
+  if (get_total(dealer_cards) > get_total(player_cards)) && (get_total(dealer_cards) < 22)
+    puts 'Dealer wins'
+  end
+
+  if get_total(dealer_cards) == get_total(player_cards)
+    puts 'Push'
+  end
+
+  if get_total(dealer_cards) > 21
+    puts 'Dealer busts, YOU WIN!'
+  end
+
+  puts 'Play again? Enter y) for yes and n) for no'
+  play_again = gets.chomp
 end
